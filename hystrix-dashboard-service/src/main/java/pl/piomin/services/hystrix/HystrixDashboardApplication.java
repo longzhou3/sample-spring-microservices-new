820ebf545a45d90a25441e491b3a8075988d8287
lz <lz@qq.com>
2018-07-12 16:48:32 +0800

    HystrixDashboardApplication 目前还有问题

---
+++ hystrix-dashboard-service/src/main/java/pl/piomin/services/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,29 @@
+package pl.piomin.services.hystrix;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(HystrixDashboardApplication.class).run(args);
+	}
+
+	/*@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}*/
+
+}

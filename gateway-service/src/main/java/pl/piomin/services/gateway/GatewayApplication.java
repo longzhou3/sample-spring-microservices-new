98eef071b36cb90cb8c62d3e77271b0b596f8b42
lz <lz@qq.com>
2018-07-12 18:04:41 +0800

    spring admin 监控提交

--- gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,5 +19,13 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-	
+
+	/**
+	 * 自定义限流标志的key，多个维度可以从这里入手
+	 * exchange对象中获取服务ID、请求信息，用户信息等
+	 */
+	@Bean
+	KeyResolver remoteAddrKeyResolver() {
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+	}
 }
